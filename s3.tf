8822a015d4055220358f21515dc6dfece83570ed
Evgeniy Akulenko <akulenko.contact@gmail.com>
2025-06-11 09:46:30 +0300

    Protect S3 Bucket with Versioning Enabled

--- s3.tf
+++ s3.tf
@@ -1,5 +1,8 @@
 resource "aws_s3_bucket" "terraform_state" {
   bucket = var.bucket_name
+  lifecycle {
+    prevent_destroy = true
+  }
 
   tags = {
     Name        = "Terraform State Bucket"
@@ -8,14 +11,14 @@ resource "aws_s3_bucket" "terraform_state" {
 }
 
 resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
-  bucket = "aws_s3_bucket.terraform_state.id"
+  bucket = aws_s3_bucket.terraform_state.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_encryption" {
-  bucket = "aws_s3_bucket.terraform_state.id"
+  bucket = aws_s3_bucket.terraform_state.id
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
